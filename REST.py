2c8be83572dab2b2c3f4fa1eed5e04f481b58172
Rojber <rm5672@gmail.com>
2020-06-20 21:32:27 +0200

    PZ

--- REST.py
+++ REST.py
@@ -251,6 +251,7 @@ def signUp():
         js = server_decryptor.decrypt(js)
         js = json_util.loads(js.decode('utf-8'))
 
+        pprint.pprint(js)
         check = db.accounts.find_one(
             {
                 'login': client_encryption.encrypt(js['login'], "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic", data_key_id)
